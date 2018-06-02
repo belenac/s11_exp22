319067ae29ffbb123425717a82c80a8ad2dd1f15
belenac <eac1992@gmail.com>
2018-06-01 20:13:57 -0400

    Segunda app, cuarta parte

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_224405) do
+ActiveRecord::Schema.define(version: 2018_06_01_235951) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
